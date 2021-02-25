4675c622be748f387a88f6aef7ace545622baec4
JhonSanz <ingjhonsanz@utp.edu.co>
2021-02-24 23:17:20 -0500

    forward chaining

--- semestre1/ia1/forward_chaining.py
+++ semestre1/ia1/forward_chaining.py
@@ -1,33 +1,60 @@
 
+class Solver:
+    a = "Tos seca"
+    b = "Fiebre"
+    c = "Malestar"
+    HYPOTESIS = "Covid 19"
+    """ Base de hechos, hechos inciales, BH """
+    INITIAL_FACTS = {a,b,c} 
+    """ Base de conocimiento, BC """
+    KNOWLEDGE_BASE = {}
 
-def resolve(conflict_set):
-    """ Resolve conflict set
-        - Choose a rule using heristic
-        - Get rule returning
-        - Put into conflict_set
-    """
-    pass
+    def heuristic(self, conflict_set):
+        """
+            - Choose a rule by any way
+        """
+        rule = list(conflict_set)[0]
+        return rule
 
-def initialize_conflict_set(b_c, b_h):
-    pass
+    def resolve(self, conflict_set):
+        """ Resolve conflict set
+            - Choose a rule using heristic
+            - Get rule result
+            - return rule result and rule
+        """
+        rule = self.heuristic(conflict_set)
+        return rule.get_result(), rule
 
-def forward_chaining(b_c, b_h, h):
-    conflict_set = initialize_conflict_set(b_c, b_h)
-    while(
-        not h in k_b and
-        conflict_set and
-    ):
-        conflict_set = 
-        if conflict_set:
-            resolve(conflict_set)
-            new_facts = 
+    def generate_conflict_set(b_c, self, b_h):
+        """ Conflict set
+            All rules that have as antecedents one or more of
+            initial facts
 
+            - Search rules that have as antecedets initals facts
+            - Put that rules into conflict set
+        """
+        pass
 
-a = "Tos seca"
-b = "Fiebre"
-c = "Malestar"
-h = "Covid 19"
-INITIAL_FACTS = {a,b,c} # Base de hechos
-KNOWLEDGE_BASE = {}
+    def forward_chaining(self):
+        visited = set()
+        conflict_set = self.generate_conflict_set(
+            self.KNOWLEDGE_BASE, self.INITIAL_FACTS
+        )
+        while(
+            not self.HYPOTESIS in self.KNOWLEDGE_BASE and
+            conflict_set and
+        ):
+            """ Updates conflict set excluding visited rules """
+            conflict_set = (
+                self.generate_conflict_set(
+                    self.KNOWLEDGE_BASE,
+                    self.INITIAL_FACTS
+                ).difference_update(visited)
+            )
+            if conflict_set:
+                fact, rule = self.resolve(conflict_set)
+                self.INITIAL_FACTS.add(fact)
+                visited.add(rule)
 
-forward_chaining(INITIAL_FACTS, KNOWLEDGE_BASE, h)
+solver = Solver()
+solver.forward_chaining()
